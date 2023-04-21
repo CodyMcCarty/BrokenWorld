0f688a67a4b886d59f04956a4aa5631e5cccb7a3
Cody McCarty <codyjmccarty@gmail.com>
2023-04-21 15:59:07 -0500

    Hud and playerController

---
+++ BWMainHUD.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/HUD.h"
+#include "BWMainHUD.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BROKENWORLD_API ABWMainHUD : public AHUD
+{
+	GENERATED_BODY()
+	
+};
