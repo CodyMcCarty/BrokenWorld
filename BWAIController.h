43d6813e04df349528240f499b3151818d7d6cb6
Cody McCarty <codyjmccarty@gmail.com>
2023-04-21 17:05:31 -0500

    BW AI Controller

---
+++ BWAIController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "BWAIController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BROKENWORLD_API ABWAIController : public AAIController
+{
+	GENERATED_BODY()
+	
+};
